c1c4c5a1a93dc553943af9dd58f41533175a4acb
malov.alexsander <malov.alexsander@yandex.ru>
2018-03-25 22:20:15 +0300

    add test new in inherit

---
+++ TestExample/TestExample/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestExample
+{
+    public class Class1
+    {
+        public Class1()
+        {
+            Console.WriteLine("This is Class1");
+        }
+    }
+
+    class Class2 : Class1
+    {
+        public Class2()
+        {
+            Console.WriteLine("This is Class2");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 class2 = new Class2();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestExample/newInherit/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newInherit
+{
+    public class A
+    {
+        public virtual void Write()
+        {
+            Console.WriteLine("This is A");
+        }
+    }
+
+    public class B : A
+    {
+        public override void Write()
+        {
+            Console.WriteLine("This is B");
+        }
+    }
+
+    public class C : A
+    {
+        public new void Write()
+        {
+            Console.WriteLine("This is C");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj1 = new A();
+            A obj2 = new B();
+            A obj3 = new C();
+
+            obj1.Write();
+            obj2.Write();
+            obj3.Write();
+
+            Console.ReadKey();
+        }
+    }
+}
